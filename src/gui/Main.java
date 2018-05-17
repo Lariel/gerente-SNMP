8f014ee2b790f4ffb30c52ca20a1d3ca13da14d5
12205112 <12205112@w1846823.portoalegre.pucrsnet.br>
2018-05-17 18:34:02 -0300

    commit inicial

---
+++ src/gui/Main.java
@@ -0,0 +1,28 @@
+package gui;
+	
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage stage) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("Tela.fxml"));
+		Scene scene = new Scene(root);
+		
+		stage.setScene(scene);
+		stage.setTitle("Gerente SNMP");
+		stage.setResizable(false);
+		stage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/gui/TelaController.java
@@ -0,0 +1,76 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class TelaController implements Initializable{
+	ObservableList<String> itenscbOperacao = FXCollections.observableArrayList("Get","GetNext","Set","GetBulk","Walk","GetTable","GetDelta");
+
+
+	@FXML // fx:id="lvMIB"
+	private ListView<?> lvMIB; // Value injected by FXMLLoader
+
+	@FXML // fx:id="cbOperacao"
+	private ComboBox<String> cbOperacao; // Value injected by FXMLLoader
+
+	@FXML // fx:id="btExecuta"
+	private Button btExecuta; // Value injected by FXMLLoader
+
+	@FXML // fx:id="miClose"
+	private MenuItem miClose; // Value injected by FXMLLoader
+
+	@FXML // fx:id="miSobre"
+	private MenuItem miSobre; // Value injected by FXMLLoader    
+
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		cbOperacao.setItems(itenscbOperacao);
+		cbOperacao.setTooltip(new Tooltip("Selecione a operação desejada"));
+
+	}
+	
+	// Ações tela
+	@FXML
+	void executar(ActionEvent event) {
+		
+	}
+	
+	// Itens de menu
+
+	@FXML
+	void sobre(ActionEvent event) {
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("Sobre");
+		alert.setHeaderText("T1 - Gerencia de Redes");
+		alert.setContentText("Desenvolvido por: \n Guilherme Dohms\n Lariel Negreiros \n \nProfessora: \n Cristina Nunes");
+		Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+		alert.showAndWait();
+	}
+
+	@FXML
+	void fechar(ActionEvent event) {
+		Platform.exit();
+	}
+
+
+}
